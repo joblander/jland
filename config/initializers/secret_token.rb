86e6baafc2a4bb529a94fcb35f3c07ad0ea2dc72
Oren Dobzinski <orend@penthera.com>
2012-01-16 13:57:19 -0500

    initial commit. empty rails 3.1.3 app with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joblander::Application.config.secret_token = 'd9c8a7b60fbeb80f91d213f961b142f7cf56c6b213de9b952e3e225a803dab11ae19e31fa2c4e03d31a2b124012d4572882e54bc759ce4ec9792037f45cb5f69'
