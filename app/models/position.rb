4e4b9b10d689aabe9285c586f1406bbef9398627
Oren Dobzinski <orend@penthera.com>
2012-02-28 23:37:31 -0500

    fixd typo in position state

--- app/models/position.rb
+++ app/models/position.rb
@@ -14,7 +14,7 @@
 
 class Position < ActiveRecord::Base
 
-  PSTATES = [:to_review, :to_apply, :applied, :to_scedule, :interviewed]
+  PSTATES = [:to_review, :to_apply, :applied, :to_schedule, :interviewed]
 
   belongs_to :user
   belongs_to :lead_search
