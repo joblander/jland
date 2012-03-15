1edfd0cd8149f2166eb87af9520d6135cbf56053
Oren Dobzinski <orend@penthera.com>
2012-03-15 15:09:56 -0400

    allow nil zipcodes

--- app/models/job_search.rb
+++ app/models/job_search.rb
@@ -1,7 +1,9 @@
 class JobSearch < ActiveRecord::Base
+  #TODO: index this field
   belongs_to :user
 
   def fetch
-    SimplyHiredSearch.search(self.search_term, :zipcode => self.zipcode.to_s)
+    SimplyHiredSearch.search(self.search_term, :zipcode => (self.zipcode.nil? ? nil : self.zipcode.to_s))
   end
+
 end
